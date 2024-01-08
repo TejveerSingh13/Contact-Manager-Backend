af8e56feca39ce97201da37f0f60c10d7aa8821d
TejveerSingh13 <tejveersingh8513@gmail.com>
2024-01-08 09:49:55 -0700

    Error handler middleware +  code formatting

--- controllers/contactControllers.js
+++ controllers/contactControllers.js
@@ -3,48 +3,48 @@
 // @desc Get all contacts
 // @routes GET /api/contacts
 // @access public
-const getContacts = (req,res) => {
-    res.status(200).json({message : "Get all Contacts"});
+const getContacts = (req, res) => {
+  res.status(200).json({ message: "Get all Contacts" });
 };
 
 // @desc Create New Contact
 // @routes POST /api/contacts
 // @access public
-const createContact = (req,res) => {
-    console.log("The req body for POST is -",req.body)
-    const {name, email, phone} = req.body
-    if(!name || !email || !phone){
-        res.status(400)
-        throw new Error("All fields are Mandatory !")
-    }
-    res.status(201).json({message : "Create Contact"})
-}
+const createContact = (req, res) => {
+  console.log("The req body for POST is -", req.body);
+  const { name, email, phone } = req.body;
+  if (!name || !email || !phone) {
+    res.status(400);
+    throw new Error("All fields are Mandatory !");
+  }
+  res.status(201).json({ message: "Create Contact" });
+};
 
 // @desc Get Contact
 // @routes GET /api/contacts/:id
 // @access public
-const getContact = (req,res) => {
-    res.status(200).json({message : `Get contact for ${req.params.id}`})
-}
+const getContact = (req, res) => {
+  res.status(200).json({ message: `Get contact for ${req.params.id}` });
+};
 
 // @desc Update Contact
 // @routes PUT /api/contacts/:id
 // @access public
-const updateContact = (req,res) => {
-    res.status(200).json({message : `Update contact for ${req.params.id}`})
-}
+const updateContact = (req, res) => {
+  res.status(200).json({ message: `Update contact for ${req.params.id}` });
+};
 
 // @desc Delete Contact
 // @routes DELETE /api/contacts/:id
 // @access public
-const deleteContact = (req,res) => {
-    res.status(200).json({message : `Delete contact for ${req.params.id}`})
-}
+const deleteContact = (req, res) => {
+  res.status(200).json({ message: `Delete contact for ${req.params.id}` });
+};
 
 module.exports = {
-    getContacts, 
-    createContact, 
-    getContact, 
-    updateContact, 
-    deleteContact
-}
+  getContacts,
+  createContact,
+  getContact,
+  updateContact,
+  deleteContact,
+};
--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const express = require("express")
-const dotenv = require("dotenv").config()
-const routes = require("./routes/contactRoute")
+const express = require("express");
+const dotenv = require("dotenv").config();
+const routes = require("./routes/contactRoute");
+const errorHandler = require("./middleware/errorHandler");
 
-const app = express()
+const app = express();
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 //a middleware to parse the datastream we recieved from a req from client on server side
-app.use(express.json())
+app.use(express.json());
 //The app.use is known as a middle ware
-app.use("/api/contacts", routes)
+app.use("/api/contacts", routes);
+app.use(errorHandler);
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}....`)
-})
+  console.log(`Server running on port ${port}....`);
+});
