f0080a5f522512e233ca5da6ee14c8735d41964b
TejveerSingh13 <tejveersingh8513@gmail.com>
2024-01-19 08:30:49 -0700

    Created user routes and controller

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
-const routes = require("./routes/contactRoute");
+const contactRoutes = require("./routes/contactRoute");
+const userRoutes = require("./routes/userRoutes");
 const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 
@@ -12,7 +13,8 @@ const port = process.env.PORT || 5000;
 //a middleware to parse the datastream we recieved from a req from client on server side
 app.use(express.json());
 //The app.use is known as a middle ware
-app.use("/api/contacts", routes);
+app.use("/api/contacts", contactRoutes);
+app.use("/api/users", userRoutes);
 app.use(errorHandler);
 
 app.listen(port, () => {
