ba3e06558d2dbdce9943912809a693df0cdcba9f
TejveerSingh13 <tejveersingh8513@gmail.com>
2024-01-20 10:27:18 -0700

    Added JWT and access token functionality

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/userModal");
 
 // @desc Register a user
@@ -40,14 +41,37 @@ const registerUser = asyncHandler(async (req, res) => {
 // @routes POST /api/user/login
 // @access public
 const loginUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Login the User" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are mandatory for Login!");
+  }
+  const user = await User.findOne({ email });
+  //compare passowrd with hashed
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user.id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "1m" }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(401);
+    throw new Error("Wmail or password not valid");
+  }
 });
 
 // @desc Current user Info
 // @routes POST /api/user/current
 // @access private
 const currentUser = asyncHandler(async (req, res) => {
-  res.json({ message: "Current user information" });
+  res.json(req.user);
 });
 
 module.exports = { registerUser, loginUser, currentUser };
